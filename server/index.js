6d00814c64c1df8f5d6733b1d25df6e0737c1a24
Febin-Joseph <febinachu123@gmail.com>
2023-08-24 18:13:55 +0530

    structured files and folders in the server and installed new packages and updated index.js in server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,16 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import helmet from 'helmet';
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 mongoose.connect("mongodb+srv://febinachu123:8CPFknul2o7nB5Ba@cluster0.8k0aahc.mongodb.net/?retryWrites=true&w=majority", {
     useNewUrlParser: true,
--- server/models/User.js
+++ server/models/User.js
@@ -1,9 +1,16 @@
 import mongoose from 'mongoose'
 
-const User = new mongoose.Schema({
-    profile: { type: String, required: true },
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        max: 50
+    },
+    password: {
+        type: String,
+        required: true
+    },
 })
 
-const UserSchema = mongoose.model('User', User);
-
-export default UserSchema;
+module.exports = mongoose.model('User', userSchema);
