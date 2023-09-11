f48aa5becb339802a0161a440f5e8b76e85f84ed
Febin-Joseph <febinachu123@gmail.com>
2023-09-11 15:15:40 +0530

    changed back hosted link

--- client/src/redux/auth/authActions.js
+++ client/src/redux/auth/authActions.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:4000';
+const API_URL = 'https://zoomify-backend.vercel.app';
 
 // Sign Up action
 export const signup = createAsyncThunk('auth/signup', async (userData) => {
