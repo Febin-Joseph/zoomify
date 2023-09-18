c1e29747bc0d3af3b4cc985d03f8e6d12ee04125
Febin-Joseph <febinachu123@gmail.com>
2023-09-18 20:02:54 +0530

    changed to SSL for WebSocket server configuration

--- client/src/utils/SocketProvider.jsx
+++ client/src/utils/SocketProvider.jsx
@@ -9,7 +9,7 @@ export const useSocket = () => {
 }
 
 export const SocketProvider = (props) => {
-    const socket = useMemo(() => io('localhost:4000'), [])
+    const socket = useMemo(() => io('https://zoomify.vercel.app'), [])
 
     return (
         <SocketContext.Provider value={socket}>
