60ab04c13da47a3bf1dfe0e76e069ac3de2d4098
Febin-Joseph <febinachu123@gmail.com>
2023-09-18 22:07:42 +0530

    modified link

--- client/src/utils/SocketProvider.jsx
+++ client/src/utils/SocketProvider.jsx
@@ -9,7 +9,7 @@ export const useSocket = () => {
 }
 
 export const SocketProvider = (props) => {
-    const socket = useMemo(() => io("https://zoomify-backend.vercel.app/"), [])
+    const socket = useMemo(() => io("http:localhost:4000"), [])
 
     return (
         <SocketContext.Provider value={socket}>
