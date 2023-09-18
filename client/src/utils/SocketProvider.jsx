15ada99cc3ff03a36e5a8802ef4692a1dedb0c62
Febin-Joseph <febinachu123@gmail.com>
2023-09-18 22:22:58 +0530

    link change after stack overflow

--- client/src/utils/SocketProvider.jsx
+++ client/src/utils/SocketProvider.jsx
@@ -9,7 +9,7 @@ export const useSocket = () => {
 }
 
 export const SocketProvider = (props) => {
-    const socket = useMemo(() => io("http://localhost:4000"), [])
+    const socket = useMemo(() => io(), [])
 
     return (
         <SocketContext.Provider value={socket}>
