8fc9b1747c043b227393ebb16b4f4de15872b6da
Febin-Joseph <febinachu123@gmail.com>
2023-09-27 11:02:50 +0530

    contributed to payment card

--- client/src/pages/pricing-plans/PaymentMethod.jsx
+++ client/src/pages/pricing-plans/PaymentMethod.jsx
@@ -10,16 +10,16 @@ const PaymentMethod = () => {
   return (
     <div>
       {isPaymentEnabled && (
-        <div className='bg-white w-[400px] md:w-[430px] h-[340px] rounded-[15px]'>
-          <p className='flex justify-center items-center text-[25px] font-bold mt-5 md:text-[28px]'>
+        <div className='text-white paymentCard w-[400px] md:w-[430px] h-[340px] rounded-[15px]'>
+          <p className='flex justify-center items-center text-[25px] font-bold pt-5 md:text-[28px]'>
             Select Your Payment Method
           </p>
           <div>
             {/* <input type="radio" /> */}
             <div className='text-black flex flex-col font-bold text-[20px] mt-4'>
-              <button className='bg-red-800 text-center h-[60px] mb-1'>Stripe</button>
-              <button className='bg-blue-800 text-center h-[60px] mb-1'>PayPal</button>
-              <button className='bg-green-800 text-center h-[60px] mb-1'>Razorpay</button>
+              <button className='paymentBtn h-[60px] mb-1'>Stripe</button>
+              <button className='paymentBtn text-center h-[60px] mb-1'>PayPal</button>
+              <button className='paymentBtn text-center h-[60px] mb-1'>Razorpay</button>
             </div>
             <div className='mt-3 space-x-6 text-white font-bold text-center'>
               <button
