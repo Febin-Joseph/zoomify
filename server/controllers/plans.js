3056869322026cef08bd2b35b5ddaeca89133003
Febin-Joseph <febinachu123@gmail.com>
2023-09-29 13:31:57 +0530

    vercel changes

--- server/controllers/plans.js
+++ server/controllers/plans.js
@@ -1,11 +1,11 @@
-// import Plan from "../models/Pricing-plans.js";
+import Plan from "../models/Pricing-plans.js";
 
-// export const pricingPlans = async (req, res) => {
-//     try {
-//         const plans = await Plan.find();
-//         res.json(plans);
-//     } catch (error) {
-//         console.error('Error fetching plans:', error);
-//         res.status(500).json({ error: error.message });
-//     }
-// };
+export const pricingPlans = async (req, res) => {
+    try {
+        const plans = await Plan.find();
+        res.json(plans);
+    } catch (error) {
+        console.error('Error fetching plans:', error);
+        res.status(500).json({ error: error.message });
+    }
+};
