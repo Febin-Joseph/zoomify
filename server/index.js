3b64c4f119e5fb6e3fe17aa55ed5fddd788d3f1b
Febin-Joseph <febinachu123@gmail.com>
2023-10-03 16:55:37 +0530

    added middleware of cors for error handling

--- server/index.js
+++ server/index.js
@@ -36,6 +36,7 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
+app.options('*', cors());
 
 //Routes
 app.use('/auth', authRoutes)
