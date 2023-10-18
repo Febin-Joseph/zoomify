88278ef5cfcdb04ef999a2ae832aa98b09c8aad8
Febin-Joseph <febinachu123@gmail.com>
2023-10-18 12:10:15 +0530

    transfering audio to each devices in the room with agora sdk

--- client/src/pages/videoCall/room/Room.jsx
+++ client/src/pages/videoCall/room/Room.jsx
@@ -3,9 +3,6 @@ import {
   AgoraVideoPlayer,
   createClient,
   createMicrophoneAndCameraTracks,
-  IAgoraRTCRemoteUser,
-  ICameraVideoTrack,
-  IMicrophoneAudioTrack,
 } from "agora-rtc-react";
 
 const Room = () => {
@@ -27,7 +24,7 @@ const Room = () => {
 const useClient = createClient({
   mode: "rtc",
   codec: "vp8",
-  appId: '7457a70d4d864646b16e8fc3f75413ff'
+  appId: "7457a70d4d864646b16e8fc3f75413ff", // Replace with your Agora App ID
 });
 
 const useMicrophoneAndCameraTracks = createMicrophoneAndCameraTracks();
@@ -37,48 +34,46 @@ const VideoCall = (props) => {
   const [users, setUsers] = useState([]);
   const [start, setStart] = useState(false);
   const client = useClient();
-  const { ready, tracks } = useMicrophoneAndCameraTracks();
+  const { ready, tracks, error } = useMicrophoneAndCameraTracks();
 
   useEffect(() => {
-    let init = async (name) => {
-      client.on("user-published", async (user, mediaType) => {
-        await client.subscribe(user, mediaType);
-        if (mediaType === "video") {
-          setUsers((prevUsers) => {
-            return [...prevUsers, user];
-          });
-        }
-        if (mediaType === "audio") {
-          user.audioTrack?.play();
-        }
-      });
+    if (error) {
+      console.error("Error accessing camera and microphone:", error);
+    } else {
+      let init = async (name) => {
+        client.on("user-published", async (user, mediaType) => {
+          await client.subscribe(user, mediaType);
+          if (mediaType === "video") {
+            setUsers((prevUsers) => {
+              return [...prevUsers, user];
+            });
+          }
+        });
 
-      client.on("user-unpublished", (user, type) => {
-        if (type === "audio") {
-          user.audioTrack?.stop();
-        }
-        if (type === "video") {
+        client.on("user-unpublished", (user, type) => {
+          if (type === "video") {
+            setUsers((prevUsers) => {
+              return prevUsers.filter((User) => User.uid !== user.uid);
+            });
+          }
+        });
+
+        client.on("user-left", (user) => {
           setUsers((prevUsers) => {
             return prevUsers.filter((User) => User.uid !== user.uid);
           });
-        }
-      });
-
-      client.on("user-left", (user) => {
-        setUsers((prevUsers) => {
-          return prevUsers.filter((User) => User.uid !== user.uid);
         });
-      });
 
-      await client.join("YOUR_APP_ID", name, null, null);
-      if (tracks) await client.publish([tracks[0], tracks[1]]);
-      setStart(true);
-    };
+        await client.join("7457a70d4d864646b16e8fc3f75413ff", name, null, null); // Replace your_token_here with your token
+        if (tracks) await client.publish([tracks[0], tracks[1]]);
+        setStart(true);
+      };
 
-    if (ready && tracks) {
-      init(channelName);
+      if (ready && tracks) {
+        init(channelName);
+      }
     }
-  }, [channelName, client, ready, tracks]);
+  }, [error, channelName, client, ready, tracks]);
 
   return (
     <div className="App">
@@ -96,11 +91,12 @@ const Videos = (props) => {
   return (
     <div>
       <div id="videos">
-        <AgoraVideoPlayer
-          className="vid"
-          videoTrack={tracks[1]}
-          style={{ height: "95%", width: "95%" }}
-        />
+        <div className="vid" style={{ height: "95%", width: "95%" }}>
+          <AgoraVideoPlayer
+            videoTrack={tracks[1]}
+            style={{ height: "100%", width: "100%" }}
+          />
+        </div>
         {users.length > 0 &&
           users.map((user) => {
             if (user.videoTrack) {
@@ -150,10 +146,10 @@ const Controls = (props) => {
   return (
     <div className="controls">
       <p className={trackState.audio ? "on" : ""} onClick={() => mute("audio")}>
-        {trackState.audio ? "MuteAudio" : "UnmuteAudio"}
+        {trackState.audio ? "Mute Audio" : "Unmute Audio"}
       </p>
       <p className={trackState.video ? "on" : ""} onClick={() => mute("video")}>
-        {trackState.video ? "MuteVideo" : "UnmuteVideo"}
+        {trackState.video ? "Mute Video" : "Unmute Video"}
       </p>
       {<p onClick={() => leaveChannel()}>Leave</p>}
     </div>
@@ -161,25 +157,25 @@ const Controls = (props) => {
 };
 
 const ChannelForm = (props) => {
-    const { setInCall, setChannelName } = props;
-  
-    return (
-      <form className="join">
-        <input
-          type="text"
-          placeholder="Enter Channel Name"
-          onChange={(e) => setChannelName(e.target.value)}
-        />
-        <button
-          onClick={(e) => {
-            e.preventDefault();
-            setInCall(true);
-          }}
-        >
-          Join
-        </button>
-      </form>
-    );
-  };
+  const { setInCall, setChannelName } = props;
+
+  return (
+    <form className="join">
+      <input
+        type="text"
+        placeholder="Enter Channel Name"
+        onChange={(e) => setChannelName(e.target.value)}
+      />
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          setInCall(true);
+        }}
+      >
+        Join
+      </button>
+    </form>
+  );
+};
 
 export default Room;
