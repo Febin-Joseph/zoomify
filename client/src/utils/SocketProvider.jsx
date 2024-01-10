2593f983081644d5e8aa4d5a68f7b3044a867b4f
FEBIN JOSEPH <128711401+Febin-Joseph@users.noreply.github.com>
2024-01-10 23:55:07 +0530

    Changed url of socket connection in SocketProvider.jsx

--- client/src/utils/SocketProvider.jsx
+++ client/src/utils/SocketProvider.jsx
@@ -9,7 +9,7 @@ export const useSocket = () => {
 }
 
 export const SocketProvider = (props) => {
-    const socket = useMemo(() => io('https://localhost:4000'), [])
+    const socket = useMemo(() => io('https://zoomify-backend.onrender.com'), [])
 
     return (
         <SocketContext.Provider value={socket}>
